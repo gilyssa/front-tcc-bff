05133aeb257ca9d42ae0ed8fa4a360dd120cfb47
gabrielgames <gabrielgames@fieg.com.br>
2022-10-06 14:52:40 -0300

    att tabela

--- src/pages/PesquisarCliente/index.js
+++ src/pages/PesquisarCliente/index.js
@@ -1,85 +1,86 @@
 import React from "react";
 import { Button, Table } from "react-bootstrap";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import * as C from "./styles";
+import TableScrollbar from "react-table-scrollbar";
 
-class PesquisarCliente extends React.Component{
+class PesquisarCliente extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-      super(props);
+    this.state = {
+      clientes: [],
+    };
+  }
+  componentDidMount() {
+    alert("Buscando lista de usuários");
+    this.buscarClientes();
+  }
+  componentWillUnmount() {
+    alert("O componente Foi desmontado!!!");
+  }
 
-      this.state = {
-        clientes:[]
-      }
-    }
-componentDidMount(){
-  alert("Buscando lista de usuários")
-  this.buscarClientes();
-}
-componentWillUnmount(){
-  alert("O componente Foi desmontado!!!")
-}
+  buscarClientes = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: JSON.parse(localStorage.getItem("signin")).signin.token,
+      },
+    };
 
-buscarClientes = () =>{
-  const options = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      "token":JSON.parse(localStorage.getItem("signin")).signin.token,
-
-    }
+    fetch("https://react-api-bff.herokuapp.com/api/clientes?", options)
+      .then((response) => response.json())
+      .then((dados) => {
+        console.log(dados);
+        this.setState({ clientes: dados });
+      });
   };
-
-  fetch("https://react-api-bff.herokuapp.com/api/clientes?",options)
-
-  .then(response => response.json())
-  .then(dados => {
-    console.log(dados);
-    this.setState({clientes: dados})
-    
-  })
-}
-deletarClientes = (id) => {
-  fetch("htpps"+id,{method: 'DELETE'})
-  .then(response =>{
-    if(response.ok){
-      this.buscarClientes();
-    }
-  } )
-  
-}
-    render(){
-      return(
-        <C.Container>
+  deletarClientes = (id) => {
+    fetch("htpps" + id, { method: "DELETE" }).then((response) => {
+      if (response.ok) {
+        this.buscarClientes();
+      }
+    });
+  };
+  render() {
+    return (
+      <C.Container>
         <C.Gabriel>
-        <label>teste</label>
-        <label>teste</label>
+          <label>teste</label>
+          <label>teste</label>
         </C.Gabriel>
         <C.Content>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <td>NOME</td>
-              <td>EMAIL</td>
-              <td>OPÇÕES</td>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.clientes.map((cliente) =>
-              <tr>
-                <td>{cliente.nome}</td>
-                <td>{cliente.email}</td>
-                <td><Button variant="danger" onClick={()=>this.deletarClientes(cliente.id)}>Excluir</Button></td>
-            </tr>
-              )
-            }  
-          </tbody>
-        </Table>
-
+          <TableScrollbar rows={5}>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <td>NOME</td>
+                  <td>EMAIL</td>
+                  <td>OPÇÕES</td>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.clientes.map((cliente) => (
+                  <tr>
+                    <td>{cliente.nome}</td>
+                    <td>{cliente.email}</td>
+                    <td>
+                      <Button
+                        variant="danger"
+                        onClick={() => this.deletarClientes(cliente.id)}
+                      >
+                        Excluir
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </TableScrollbar>
         </C.Content>
-        </C.Container>
-      );
-    }
+      </C.Container>
+    );
+  }
 }
 export default PesquisarCliente;
--- src/pages/Procedimentos/ListarProcedimentos/index.js
+++ src/pages/Procedimentos/ListarProcedimentos/index.js
@@ -1,111 +1,106 @@
 import React from "react";
 import { Button, Table } from "react-bootstrap";
-import {Form} from "react-bootstrap";
-import { useState } from 'react';
+import { Form } from "react-bootstrap";
+import { useState } from "react";
+import TableScrollbar from "react-table-scrollbar";
+import * as C from "./styles";
 
-class ListarProcedimentos extends React.Component{
+class ListarProcedimentos extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor (props){
-        super(props);
-
-        this.state= {
-            procedimentos: [],
-            nome: 'gabr'
-        }
-    }
-    componentDidMount(){
-        this.ListarProcedimentos();
-      }
-      componentWillUnmount(){
-        
-      }
-      ListarProcedimentos = () =>{
-        const options = {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            "token":JSON.parse(localStorage.getItem("signin")).signin.token,
-            "usuario_id": JSON.parse(localStorage.getItem("signin")).signin.usuario,
-      
-          }
-        };
-        fetch("https://react-api-bff.herokuapp.com/api/procedimentos?",options)
-      
-        .then(response => response.json())
-        .then(dados => {
-          console.log(dados);
-          this.setState({procedimentos: dados})
-          
-        });
-    }
-    carregarProcedimento = (id) =>{
-        const options = {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              "token":JSON.parse(localStorage.getItem("signin")).signin.token,
-              "usuario_id": JSON.parse(localStorage.getItem("signin")).signin.usuario,
-            }
-          };
-          fetch("https://react-api-bff.herokuapp.com/api/procedimentos?"+id,options)
-        
-          .then(response => response.json())
-          .then(procedimentos => {
-            this.setState({
-                id: procedimentos.id,
-                nome: procedimentos.nome
-            })
-            
-          });
-    }
-    
-    atualizaNome = (e) =>{
+    this.state = {
+      procedimentos: [],
+      nome: "gabr",
+    };
+  }
+  componentDidMount() {
+    this.ListarProcedimentos();
+  }
+  componentWillUnmount() {}
+  ListarProcedimentos = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: JSON.parse(localStorage.getItem("signin")).signin.token,
+        usuario_id: JSON.parse(localStorage.getItem("signin")).signin.usuario,
+      },
+    };
+    fetch("https://react-api-bff.herokuapp.com/api/procedimentos?", options)
+      .then((response) => response.json())
+      .then((dados) => {
+        console.log(dados);
+        this.setState({ procedimentos: dados });
+      });
+  };
+  carregarProcedimento = (id) => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        token: JSON.parse(localStorage.getItem("signin")).signin.token,
+        usuario_id: JSON.parse(localStorage.getItem("signin")).signin.usuario,
+      },
+    };
+    fetch(
+      "https://react-api-bff.herokuapp.com/api/procedimentos?" + id,
+      options
+    )
+      .then((response) => response.json())
+      .then((procedimentos) => {
         this.setState({
-            nome: e.target.value
-        })
+          id: procedimentos.id,
+          nome: procedimentos.nome,
+        });
+      });
+  };
+
+  atualizaNome = (e) => {
+    this.setState({
+      nome: e.target.value,
+    });
+  };
+  submit = () => {
+    {
+      const procedimento = {
+        nome: this.state.nome,
+      };
     }
-    submit = () =>{{
-        const procedimento = {
-            nome: this.state.nome
-        }
-    }}
-    render(){
-        return(
-            <div>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Procedimento</Form.Label>
-                    <Form.Control type="text" placeholder="Procedimento" value={this.state.nome} onChange={this.atualizaNome}/>
-                </Form.Group>
-
-                <Button variant="primary" onClick={this.submit}>
-                    Salvar
-                </Button>
-            </Form>
-
-            
-                    <br></br>
-            <Table striped bordered hover>
-                <thead>
+  };
+  render() {
+    return (
+      <div>
+        <C.Container>
+          <C.Gabriel>
+            <label>teste</label>
+            <label>teste</label>
+          </C.Gabriel>
+          <C.Content>
+            <C.Gabriel>
+              <TableScrollbar rows={5} height="200px">
+                <Table striped bordered hover>
+                  <thead>
                     <tr>
-                        <th>Procedimento</th>
-                        <th>OPÇÕES</th>
+                      <td>NOME</td>
+                      <td>EMAIL</td>
                     </tr>
-                </thead>
-                <tbody>
-                    {
-                this.state.procedimentos.map((procedimentos) =>
-                <tr>
-                    <td>{procedimentos.nome}</td>
-                    <td><Button variant="secondary" onClick={()=>this.carregarProcedimento(procedimentos.id)}>Atualizar</Button></td>
-                </tr>
-                )
-                }
-                </tbody>
-            </Table>
-            </div>
-        )
-    }
+                  </thead>
+                  <tbody>
+                    {this.state.procedimentos.map((procedimentos) => (
+                      <tr>
+                        <td>{procedimentos.nome}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </TableScrollbar>
+            </C.Gabriel>
+          </C.Content>
+        </C.Container>
+      </div>
+    );
+  }
 }
 
 export default ListarProcedimentos;
